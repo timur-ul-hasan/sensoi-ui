581e2863d7d03c6ca73a1c814a9b1e8005390dc0
Hasan Javed <hasanjaved065@gmail.com>
2020-08-26 23:05:14 +0500

    Fix urls

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -2,14 +2,15 @@ import React, { memo, useEffect } from "react";
 
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 
-import Auth from "../../routes/auth";
+import { Signin, Signup } from "../../routes/auth";
 import Dashboard from "../../routes/home/index";
 
 const App = props => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/login" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
         <Route path="/" component={Dashboard} />
       </Switch>
     </BrowserRouter>
--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,19 +1,4 @@
-import React from "react";
-import styled from "styled-components";
-import { Redirect, Route, Switch, useHistory, useLocation, useRouteMatch } from "react-router-dom";
 import Signin from "./signIn";
 import Signup from "./signUp";
 
-export default ({ match }) => {
-  const location = useLocation();
-  const signOut = useRouteMatch(`${match.url}/signout`);
-
-  return (
-    <>
-      <Switch>
-        <Route path={`${match.url}/login`} component={Signin} />
-        <Route path={`${match.url}/signup`} component={Signup} />
-      </Switch>
-    </>
-  );
-};
+export { Signin, Signup };
