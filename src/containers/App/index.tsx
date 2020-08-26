f1b5ba52e910a708cd381ac21fdcdfa9f71753a6
Hasan Javed <hasanjaved065@gmail.com>
2020-08-26 17:35:33 +0500

    Code cleanup

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,16 +1,10 @@
 import React, { memo, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  Route,
-  Switch,
-  useHistory,
-  BrowserRouter,
-  useLocation,
-  useRouteMatch,
-} from "react-router-dom";
+
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 import Auth from "../../routes/auth";
 import Dashboard from "../../routes/home/index";
+
 const App = props => {
   return (
     <BrowserRouter>
