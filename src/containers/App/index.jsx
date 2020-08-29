fe50149bcb63f7a5d1cad30833fd22afa4173119
Hasan Javed <hasanjaved065@gmail.com>
2020-08-30 00:22:38 +0500

    Removing ts file

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,23 +1,20 @@
 import React, { memo, useEffect } from "react";
-import {
-  Route,
-  Switch,
-  useHistory,
-  BrowserRouter,
-  useLocation,
-  useRouteMatch,
-} from "react-router-dom";
-
-// import Auth from "../../routes/auth/index";
 import Home from "../../routes/home/index";
 import DashBoard from "../../routes/dashboard/index";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
+import { Signin, Signup } from "../../routes/auth";
+import Dashboard from "../../routes/home/index";
+
 const App = props => {
   return (
     <BrowserRouter>
       <Switch>
-        {/* <Route path="/auth" component={Auth} /> */}
         <Route path="/home" component={Home} />
         <Route path="/dashboard" component={DashBoard} />
+        <Route path="/login" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/" component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
