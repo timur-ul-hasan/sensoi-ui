55bd1dfa0463966c4c1445d1bec2b09c273fec01
Hasan Javed <hasanjaved065@gmail.com>
2020-09-03 17:56:53 +0500

    Intermediate commit

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -8,19 +8,17 @@ import {
   useRouteMatch,
 } from "react-router-dom";
 
-import Auth from "../../routes/auth";
 import Home from "../../routes/home/index";
-import DashBoard from "../../routes/dashboard/index";
+import Dashboard from "../../routes/dashboard/index";
 
 import { Signin, Signup } from "../../routes/auth";
-import Dashboard from "../../routes/home/index";
 
 const App = props => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/home" component={Home} />
-        <Route path="/dashboard" component={DashBoard} />
+        <Route path="/dashboard" component={Dashboard} />
         <Route path="/login" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/" component={Dashboard} />
